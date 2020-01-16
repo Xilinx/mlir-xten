c306d9b1a30b9f7b58dbfc290454cb28d4493bac
Jeff Fifield <jeff.fifield@xilinx.com>
2020-01-16 20:18:41 +0000

    Fix op counting bug in max_pool2d and cleanup code

--- pytorch_mlir/dialect/csrc/ATenDialectOpStats.cpp
+++ pytorch_mlir/dialect/csrc/ATenDialectOpStats.cpp
@@ -28,6 +28,15 @@ uint64_t getTensorVolume(Type ty) {
   }
 }
 
+std::vector<uint64_t> unpackListConstant(Value *op) {
+  std::vector<uint64_t> v;
+  auto co = cast<xilinx::aten::ConstantOp>(op->getDefiningOp());
+  DenseElementsAttr a = co.template getAttrOfType<DenseElementsAttr>("value");
+  for (auto i : a.getIntValues())
+    v.push_back(i.getSExtValue());
+  return v;
+};
+
 } // namespace
 
 namespace xilinx {
@@ -38,43 +47,40 @@ std::map<std::string, uint64_t> AddOp::updateStatistics() {
 
   std::map<std::string, uint64_t> toReturn;
 
-  Type resultTy = getResult()->getType();
+  TensorType resultTy = getResult()->getType().cast<TensorType>();
+  TensorType aType = getOperand(0)->getType().cast<TensorType>();
+  TensorType bType = getOperand(1)->getType().cast<TensorType>();
+
   uint64_t ofm_volume = getTensorVolume(resultTy);
+
   toReturn["+"] = ofm_volume;
   toReturn["activation_out"] = ofm_volume;
 
-  TensorType tensorResultTy = resultTy.cast<TensorType>();
-  uint64_t num_output_neurons = tensorResultTy.getShape()[1];
-
   // Find the size of the A and B operands
-  Type aType = getOperand(0)->getType();
   uint64_t a_volume = getTensorVolume(aType);
-
-  Type bType = getOperand(1)->getType();
   uint64_t b_volume = getTensorVolume(bType);
 
   toReturn["activation_in"] = a_volume + b_volume;
   return toReturn;
+
 }
 
 // add_
 std::map<std::string, uint64_t> AddUnderOp::updateStatistics() {
+
   std::map<std::string, uint64_t> toReturn;
 
-  Type resultTy = getResult()->getType();
+  TensorType resultTy = getResult()->getType().cast<TensorType>();
+  TensorType aType = getOperand(0)->getType().cast<TensorType>();
+  TensorType bType = getOperand(1)->getType().cast<TensorType>();
+
   uint64_t ofm_volume = getTensorVolume(resultTy);
 
   toReturn["+"] = ofm_volume;
   toReturn["activation_out"] = ofm_volume;
 
-  TensorType tensorResultTy = resultTy.cast<TensorType>();
-  uint64_t num_output_neurons = tensorResultTy.getShape()[1];
-
   // Find the size of the A and B operands
-  Type aType = getOperand(0)->getType();
   uint64_t a_volume = getTensorVolume(aType);
-
-  Type bType = getOperand(1)->getType();
   uint64_t b_volume = getTensorVolume(bType);
 
   toReturn["activation_in"] = a_volume + b_volume;
@@ -85,25 +91,24 @@ std::map<std::string, uint64_t> AddUnderOp::updateStatistics() {
 std::map<std::string, uint64_t> AddmmOp::updateStatistics() {
 
   std::map<std::string, uint64_t> toReturn;
+
   // For linear, we need the number of output neurons and the number of input neurons
   // Then the number of forward MACs is input * output
   // And the number of adds is output if there is bias
 
-  Type resultTy = getResult()->getType();
-  TensorType tensorResultTy = resultTy.cast<TensorType>();
-  uint64_t num_output_neurons = tensorResultTy.getShape()[1];
-  uint64_t ofm_volume = getTensorVolume(tensorResultTy);
+  TensorType resultTy = getResult()->getType().cast<TensorType>();
+  TensorType inputTy = getOperand(1)->getType().cast<TensorType>();
+  TensorType weightTy = getOperand(2)->getType().cast<TensorType>();
+
+  uint64_t num_output_neurons = resultTy.getShape()[1];
+  uint64_t ofm_volume = getTensorVolume(resultTy);
 
   // Use the weight tensor to find the number of input neurons
-  Type wType = getOperand(2)->getType();
-  TensorType wTy = wType.cast<TensorType>();
-  uint64_t num_input_neurons = wTy.getShape()[0];
+  uint64_t num_input_neurons = weightTy.getShape()[0];
   uint64_t total_MACs = ofm_volume * num_input_neurons;
-  uint64_t weight_volume = getTensorVolume(wTy);
+  uint64_t weight_volume = getTensorVolume(weightTy);
 
-  Type ifmType = getOperand(1)->getType();
-  TensorType txTy = ifmType.cast<TensorType>();
-  uint64_t ifm_volume = getTensorVolume(txTy);
+  uint64_t ifm_volume = getTensorVolume(inputTy);
 
   toReturn["MAC"] = total_MACs;
   toReturn["+"] = ofm_volume;   // Should be gated on whether there is bias at all
@@ -115,17 +120,18 @@ std::map<std::string, uint64_t> AddmmOp::updateStatistics() {
 }
 
 // batch_norm
-std::map<std::string, uint64_t>  BatchNormOp::updateStatistics() {
-  std::map<std::string, uint64_t> toReturn;
-  Type resultTy = getResult(0)->getType();
-  TensorType tensorResultTy = resultTy.cast<TensorType>();
+std::map<std::string, uint64_t> BatchNormOp::updateStatistics() {
 
-  uint64_t op_volume = getTensorVolume(tensorResultTy);
+  std::map<std::string, uint64_t> toReturn;
+
+  TensorType resultTy = getResult(0)->getType().cast<TensorType>();
+
+  uint64_t op_volume = getTensorVolume(resultTy);
   toReturn["activation_in"] = op_volume;
   toReturn["activation_out"] = op_volume;
 
   // There are 2x as many parameters are there are planes ...
-  uint64_t ifm_depth = tensorResultTy.getShape()[1];
+  uint64_t ifm_depth = resultTy.getShape()[1];
   toReturn["parameters_in"] = ifm_depth * 2;
 
   // Now for the arithmetic.  Assume variance is calculated as sum of squares
@@ -194,33 +200,23 @@ std::map<std::string, uint64_t> ConvolutionOp::updateStatistics() {
 // max_pool2d
 std::map<std::string, uint64_t> MaxPool2dOp::updateStatistics() {
 
-  auto unpack = [](auto &op, auto &v) -> void {
-                  auto co = cast<xilinx::aten::ConstantOp>(op->getDefiningOp());
-                  DenseElementsAttr a = co.template getAttrOfType<DenseElementsAttr>("value");
-                  for (auto i : a.getIntValues())
-                    v.push_back(i.getSExtValue());
-                };
+  std::map<std::string, uint64_t> toReturn;
 
-  std::map<std::string, uint64_t>  toReturn;
-  Type resultTy = getResult()->getType();
-  TensorType tensorResultTy = resultTy.cast<TensorType>();
+  TensorType resultTy = getResult()->getType().cast<TensorType>();
+  TensorType inputType = getOperand(0)->getType().cast<TensorType>();
 
-  uint64_t ofm_volume = getTensorVolume(tensorResultTy);
+  uint64_t ofm_volume = getTensorVolume(resultTy);
   toReturn["activation_out"] = ofm_volume;
 
-  Type ifmType = getOperand(0)->getType();
-  TensorType txTy = ifmType.cast<TensorType>();
-  uint64_t ifm_volume = getTensorVolume(txTy);
+  uint64_t ifm_volume = getTensorVolume(inputType);
   toReturn["activation_in"] = ifm_volume;
 
   // To find the number of compares, we need the filter extent
 
-  std::vector<uint64_t> kernel;
-  mlir::Value *k = getOperand(1);
-  unpack(k, kernel);
+  std::vector<uint64_t> kernel_size = unpackListConstant(getOperand(1));
 
-  uint64_t aperture = kernel[0] * kernel[1];
-  toReturn[">"] = ofm_volume * aperture;
+  uint64_t aperture = kernel_size[0] * kernel_size[1];
+  toReturn[">"] = ofm_volume * (aperture-1);
 
   return toReturn;
 }
@@ -228,54 +224,44 @@ std::map<std::string, uint64_t> MaxPool2dOp::updateStatistics() {
 // max_pool2d_with_indices
 std::map<std::string, uint64_t> MaxPool2dWithIndicesOp::updateStatistics() {
 
-  auto unpack = [](auto &op, auto &v) -> void {
-                  auto co = cast<xilinx::aten::ConstantOp>(op->getDefiningOp());
-                  DenseElementsAttr a = co.template getAttrOfType<DenseElementsAttr>("value");
-                  for (auto i : a.getIntValues())
-                    v.push_back(i.getSExtValue());
-                };
+  std::map<std::string, uint64_t> toReturn;
 
-  std::map<std::string, uint64_t>  toReturn;
-  Type resultTy = getResult(0)->getType();
-  TensorType tensorResultTy = resultTy.cast<TensorType>();
+  TensorType resultTy = getResult(0)->getType().cast<TensorType>();
+  TensorType inputType = getOperand(0)->getType().cast<TensorType>();
 
-  uint64_t ofm_volume = getTensorVolume(tensorResultTy);
+  uint64_t ofm_volume = getTensorVolume(resultTy);
   toReturn["activation_out"] = ofm_volume;
 
-  Type ifmType = getOperand(0)->getType();
-  TensorType txTy = ifmType.cast<TensorType>();
-  uint64_t ifm_volume = getTensorVolume(txTy);
+  uint64_t ifm_volume = getTensorVolume(inputType);
   toReturn["activation_in"] = ifm_volume;
 
   // To find the number of compares, we need the filter extent
 
-  std::vector<uint64_t> kernel;
-  mlir::Value *k = getOperand(1);
-  unpack(k, kernel);
+  std::vector<uint64_t> kernel_size = unpackListConstant(getOperand(1));
 
-  uint64_t aperture = kernel[0] * kernel[1];
-  toReturn[">"] = ofm_volume * aperture;
+  uint64_t aperture = kernel_size[0] * kernel_size[1];
+  toReturn[">"] = ofm_volume * (aperture-1);
 
   return toReturn;
 }
 
 // mul
 std::map<std::string, uint64_t> MulOp::updateStatistics() {
+
   std::map<std::string, uint64_t> toReturn;
 
-  Type resultTy = getResult()->getType();
+  TensorType resultTy = getResult()->getType().cast<TensorType>();
+  TensorType aType = getOperand(0)->getType().cast<TensorType>();
+  TensorType bType = getOperand(1)->getType().cast<TensorType>();
+
   uint64_t ofm_volume = getTensorVolume(resultTy);
   toReturn["+"] = ofm_volume;
   toReturn["activation_out"] = ofm_volume;
 
-  TensorType tensorResultTy = resultTy.cast<TensorType>();
-  uint64_t num_output_neurons = tensorResultTy.getShape()[1];
+  uint64_t num_output_neurons = resultTy.getShape()[1];
 
   // Find the size of the A and B operands
-  Type aType = getOperand(0)->getType();
   uint64_t a_volume = getTensorVolume(aType);
-
-  Type bType = getOperand(1)->getType();
   uint64_t b_volume = getTensorVolume(bType);
 
   toReturn["activation_in"] = a_volume + b_volume;
@@ -284,21 +270,21 @@ std::map<std::string, uint64_t> MulOp::updateStatistics() {
 
 // mul_
 std::map<std::string, uint64_t> MulUnderOp::updateStatistics() {
+
   std::map<std::string, uint64_t> toReturn;
 
-  Type resultTy = getResult()->getType();
+  TensorType resultTy = getResult()->getType().cast<TensorType>();
+  TensorType aType = getOperand(0)->getType().cast<TensorType>();
+  TensorType bType = getOperand(1)->getType().cast<TensorType>();
+
   uint64_t ofm_volume = getTensorVolume(resultTy);
   toReturn["+"] = ofm_volume;
   toReturn["activation_out"] = ofm_volume;
 
-  TensorType tensorResultTy = resultTy.cast<TensorType>();
-  uint64_t num_output_neurons = tensorResultTy.getShape()[1];
+  uint64_t num_output_neurons = resultTy.getShape()[1];
 
   // Find the size of the A and B operands
-  Type aType = getOperand(0)->getType();
   uint64_t a_volume = getTensorVolume(aType);
-
-  Type bType = getOperand(1)->getType();
   uint64_t b_volume = getTensorVolume(bType);
 
   toReturn["activation_in"] = a_volume + b_volume;
@@ -307,16 +293,17 @@ std::map<std::string, uint64_t> MulUnderOp::updateStatistics() {
 
 // native_batch_norm
 std::map<std::string, uint64_t> NativeBatchNormOp::updateStatistics() {
-  std::map<std::string, uint64_t>  toReturn;
-  Type resultTy = getResult(0)->getType();
-  TensorType tensorResultTy = resultTy.cast<TensorType>();
 
-  uint64_t op_volume = getTensorVolume(tensorResultTy);
+  std::map<std::string, uint64_t> toReturn;
+
+  TensorType resultTy = getResult(0)->getType().cast<TensorType>();
+
+  uint64_t op_volume = getTensorVolume(resultTy);
   toReturn["activation_in"] = op_volume;
   toReturn["activation_out"] = op_volume;
 
   // There are 2x as many parameters are there are planes ...
-  uint64_t ifm_depth = tensorResultTy.getShape()[1];
+  uint64_t ifm_depth = resultTy.getShape()[1];
   toReturn["parameters_in"] = ifm_depth * 2;
 
   // Now for the arithmetic.  Assume variance is calculated as sum of squares
@@ -343,11 +330,12 @@ std::map<std::string, uint64_t> NativeBatchNormOp::updateStatistics() {
 
 // relu
 std::map<std::string, uint64_t> ReLUOp::updateStatistics() {
-  std::map<std::string, uint64_t> toReturn;
-  Type resultTy = getResult()->getType();
-  TensorType tensorResultTy = resultTy.cast<TensorType>();
 
-  uint64_t op_volume = getTensorVolume(tensorResultTy);
+  std::map<std::string, uint64_t> toReturn;
+
+  Type resultTy = getResult()->getType().cast<TensorType>();
+
+  uint64_t op_volume = getTensorVolume(resultTy);
   toReturn["activation_in"] = op_volume;
   toReturn["activation_out"] = op_volume;
   toReturn[">"] = op_volume;
@@ -357,11 +345,12 @@ std::map<std::string, uint64_t> ReLUOp::updateStatistics() {
 
 // relu_
 std::map<std::string, uint64_t> ReLUUnderOp::updateStatistics() {
-  std::map<std::string, uint64_t> toReturn;
-  Type resultTy = getResult()->getType();
-  TensorType tensorResultTy = resultTy.cast<TensorType>();
 
-  uint64_t op_volume = getTensorVolume(tensorResultTy);
+  std::map<std::string, uint64_t> toReturn;
+
+  Type resultTy = getResult()->getType().cast<TensorType>();
+
+  uint64_t op_volume = getTensorVolume(resultTy);
   toReturn["activation_in"] = op_volume;
   toReturn["activation_out"] = op_volume;
   toReturn[">"] = op_volume;
