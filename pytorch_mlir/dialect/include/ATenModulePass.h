4dd36b35ff589ef7bc629755994deedf95dfe05d
Jeff Fifield <jeff.fifield@xilinx.com>
2020-02-21 16:18:27 +0000

    Add an empty module pass

---
+++ pytorch_mlir/dialect/include/ATenModulePass.h
@@ -0,0 +1,14 @@
+// (c) Copyright 2020 Xilinx Inc. All Rights Reserved.
+#pragma once
+
+namespace mlir {
+class Pass;
+} // namespace mlir
+
+namespace xilinx {
+namespace aten {
+
+std::unique_ptr<mlir::Pass> createATenModulePass();
+
+} // namespace aten
+} // namespace xilinx
