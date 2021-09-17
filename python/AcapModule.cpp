41b674009520a1973b766d908209c0feeb28229f
Jeff Fifield <jeff.fifield@xilinx.com>
2021-09-16 22:07:31 -0600

    Clean unrelated passes from xten
    Removed acap-herd-lowering, acap-loop-lowering,
    aten-simple-allocate, duplicate liveness analysis code.

--- python/AcapModule.cpp
+++ python/AcapModule.cpp
@@ -1,47 +1,18 @@
-// (c) Copyright 2019 Xilinx Inc. All Rights Reserved.
-
-// LLVM and MLIR headers must come first
-#include "llvm/Support/Debug.h"
-#include "llvm/Support/MemoryBuffer.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Support/SourceMgr.h"
-
-#include "mlir/Conversion/SCFToStandard/SCFToStandard.h"
-#include "mlir/IR/MLIRContext.h"
-#include "mlir/IR/BuiltinOps.h"
-#include "mlir/IR/Verifier.h"
-#include "mlir/Parser.h"
-#include "mlir/Pass/Pass.h"
-#include "mlir/Pass/PassManager.h"
-#include "mlir/Transforms/Passes.h"
-#include "mlir/InitAllPasses.h"
-
-#include "npcomp/Dialect/ATen/IR/ATenDialect.h"
-#include "npcomp/Dialect/ATen/Transforms/Passes.h"
+// (c) Copyright 2021 Xilinx Inc. All Rights Reserved.
 
 #include "xten/Transform/Passes.h"
 #include "xten/Conversion/Passes.h"
-
-#include "xten/Transform/ATenOpReport.h"
-#include "xten/Transform/LivenessReport.h"
 
 #include <string>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-using namespace llvm;
-
-namespace llvm {
-extern bool DebugFlag;
-}
-
 namespace xten {
 namespace {
 
 void InitXTenModuleBindings(pybind11::module m)
 {
-
   m.def("_register_all_passes", []() {
     xilinx::xten::registerTransformPasses();
     xilinx::xten::registerConversionPasses();
