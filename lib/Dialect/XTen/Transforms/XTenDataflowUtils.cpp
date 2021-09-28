1d9c01888088202252c1e0270d4cc4943ca15d9d
Kristof Denolf <kristof@xilinx.com>
2021-09-28 10:35:06 -0700

    adding headers

--- lib/Dialect/XTen/Transforms/XTenDataflowUtils.cpp
+++ lib/Dialect/XTen/Transforms/XTenDataflowUtils.cpp
@@ -1,3 +1,13 @@
+//===- XTenDataflowUtils.cpp ------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2021 Xilinx Inc.
+//
+//===----------------------------------------------------------------------===//
+
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/OperationSupport.h"
