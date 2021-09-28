d4158f1361258c1a71c4df383f55a3ee34a07292
Kristof Denolf <kristof@xilinx.com>
2021-09-28 10:31:43 -0700

    adding headers

--- include/xten/Util/Util.h
+++ include/xten/Util/Util.h
@@ -1,3 +1,13 @@
+//===- Util.h ---------------------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2021 Xilinx Inc.
+//
+//===----------------------------------------------------------------------===//
+
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/BuiltinTypes.h"
