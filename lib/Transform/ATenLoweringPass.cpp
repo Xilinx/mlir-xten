ec112e7c361a37fd7238dca4c6fd84dd119f6b2a
Kristof Denolf <kristof@xilinx.com>
2021-09-28 10:36:28 -0700

    adding headers

--- lib/Transform/ATenLoweringPass.cpp
+++ lib/Transform/ATenLoweringPass.cpp
@@ -1,4 +1,12 @@
-// (c) Copyright 2019 Xilinx Inc. All Rights Reserved.
+//===- ATenLoweringPass.cpp -------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2019 Xilinx Inc.
+//
+//===----------------------------------------------------------------------===//
 
 #include "xten/Transform/ATenLoweringPass.h"
 #include "npcomp/Dialect/ATen/IR/ATenDialect.h"
