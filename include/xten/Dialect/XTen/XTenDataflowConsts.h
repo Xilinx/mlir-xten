84f26448e2130ed253545a65894f232bb10592ea
Kristof Denolf <kristof@xilinx.com>
2021-09-28 10:29:54 -0700

    adding headers

--- include/xten/Dialect/XTen/XTenDataflowConsts.h
+++ include/xten/Dialect/XTen/XTenDataflowConsts.h
@@ -1,3 +1,13 @@
+//===- XTenDataflowConsts.h -------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2021 Xilinx Inc.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef CONSTS_H
 #define CONSTS_H
 
--- include/xten/Dialect/XTen/XTenOps.h
+++ include/xten/Dialect/XTen/XTenOps.h
@@ -1,4 +1,12 @@
-// (c) Copyright 2021 Xilinx Inc. All Rights Reserved.
+//===- XTenOps.h ------------------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2021 Xilinx Inc.
+//
+//===----------------------------------------------------------------------===//
 
 #ifndef XTENOPS_H
 #define XTENOPS_H
