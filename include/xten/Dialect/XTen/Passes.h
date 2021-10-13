584cb88f7fdd905980aee295673ec189eef902a4
Jeff Fifield <jeff.fifield@xilinx.com>
2021-10-13 10:13:52 -0600

    Fix headers removed during rebase

--- include/xten/Dialect/XTen/Passes.h
+++ include/xten/Dialect/XTen/Passes.h
@@ -1,3 +1,13 @@
+//===- Passes.h -------------------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2019 Xilinx Inc.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef XTEN_PASSES_H
 #define XTEN_PASSES_H
 
