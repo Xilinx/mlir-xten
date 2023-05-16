4ee205f9e62e1a3ec80d0ec8e554b2b50e7e752c
Dominik Montada <dominik.montada@amd.com>
2023-05-16 11:55:01 +0000

    refactor(XTenNN): FXML-1981 use new pass declaration style

--- include/xten/Dialect/XTenNN/Transforms/Simplify.h
+++ include/xten/Dialect/XTenNN/Transforms/Simplify.h
@@ -1,14 +1,14 @@
-//===- XTenNNPasses.h -------------------------------------------*- C++ -*-===//
+//===- Simplify.h -----------------------------------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// (c) Copyright 2022 Advanced Micro Devices, Inc.
+// (c) Copyright 2023 Advanced Micro Devices, Inc.
 //
 //===----------------------------------------------------------------------===//
 //
-// Declares the XTenNN pass entry points.
+// Declares the Simplify pass.
 //
 //===----------------------------------------------------------------------===//
 
@@ -18,25 +18,17 @@
 #include "xten/Dialect/XTenNN/IR/XTenNN.h"
 
 namespace mlir {
-
 class RewritePatternSet;
-
 } // namespace mlir
 
 namespace amd::xten_nn {
+
+#define GEN_PASS_DECL_XTENNNSIMPLIFY
+#include "xten/Dialect/XTenNN/Transforms/Passes.h.inc"
 
 /// Obtains the graph simplification patterns.
 void populateSimplifyPatterns(mlir::RewritePatternSet &patterns);
 
 /// Creates the graph simplification pass.
 std::unique_ptr<mlir::Pass> createSimplifyPass();
-
-//===----------------------------------------------------------------------===//
-// Registration
-//===----------------------------------------------------------------------===//
-
-/// Generate the code for registering passes.
-#define GEN_PASS_REGISTRATION
-#include "xten/Dialect/XTenNN/Transforms/XTenNNPasses.h.inc"
-
 } // namespace amd::xten_nn
